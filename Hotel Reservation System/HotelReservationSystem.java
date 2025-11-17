789d1df764be58965ecdc81f76f3ce5cb88530cf
saggy-exe <ghoshsagnic04@gmail.com>
2025-11-18 00:38:21 +0530

    Code added

--- Hotel Reservation System/HotelReservationSystem.java
+++ Hotel Reservation System/HotelReservationSystem.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 public class HotelReservationSystem {
     private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
     private static final String username = "root";
-    private static final String password = "Admin@123";
+    private static final String password = "Auror#04";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         try{
